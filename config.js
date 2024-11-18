0c1bd5c4d793c67e374a0b72be7428ad6a2426a1
Ramakrishnan <144512853+krishevolve@users.noreply.github.com>
2024-11-18 21:19:36 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = [
+    {
+        usertoken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NzM1ODIyOTQ4NzM4Yjc0MDBhODQ3MWMiLCJwdWJsaWNBZGRyZXNzIjoiY0VOYktrYzI5NzhydzFaaXNXYkFzcmc1WUoydlB2emJMOHdWYm9lS3RMeSIsIndhbGxldFR5cGUiOiJzb2xhbmEiLCJpYXQiOjE3MzE5Mzg0OTAsImV4cCI6MTc2MzQ5NjA5MH0.DVyXUVtWvVTnUZqEQ-5cIorTKer5eCJraCCjgBluGIs',
+        nodes: [
+            { nodeId: '12D3KooWReoVojGZqaCL9xmK9Wt7cvYqH9mAxGSUAYSrzUyijibW', hardwareId: 'fd7ee5bb65e41c75b30fa1c86a84e913ede93be3b95de89e919cd3f713824ea3', proxy: 'http://ehiasqrg:rgi9zyr1768c@207.244.217.165:6712' },
+			{ nodeId: '12D3KooWD1y78yniPzWfqDKc5HBscaiAyvU9Z1TbxvWVs695LZMX', hardwareId: 'b9d64744b9738f7f8a7d098273ffbfa3b9535a07e950ada2bcf6d42e04655cdc', proxy: 'http://ehiasqrg:rgi9zyr1768c@198.23.239.134:6540' }
+        ]
+    }
+    // Add more usertokens as needed
+];
